f9a0a3132a2e95574a653cea67f664716530380d
Roberto Orellana Tamayo <roberto.orellana.t@usach.cl>
2026-05-13 16:49:29 -0400

    env ip_public

--- src/services/keycloak.js
+++ src/services/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://localhost:9090",
+  url: `http://${import.meta.env.VITE_BACKEND_SERVER}:${import.meta.env.VITE_KEYCLOAK_PORT}`,
   realm: "travel-realm",
   clientId: "travel-frontend",
 });
