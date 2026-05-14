26df8bde253297f9cce35899c50fcda0e93942ea
Roberto Orellana Tamayo <roberto.orellana.t@usach.cl>
2026-05-14 17:15:49 -0400

    env produccion y desarrollo

--- src/http-common.js
+++ src/http-common.js
@@ -1,24 +1,23 @@
 import axios from "axios";
 import keycloak from "./services/keycloak";
 
-const payrollBackendServer = import.meta.env.VITE_BACKEND_SERVER;
-const payrollBackendPort = import.meta.env.VITE_BACKEND_PORT;
+const travelApi = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: `http://${payrollBackendServer}:${payrollBackendPort}`,
+    baseURL: travelApi,
     headers: {
         'Content-Type': 'application/json'
     }
 });
 
 api.interceptors.request.use(async (config) => {
-  if (keycloak.authenticated) {
-    await keycloak.updateToken(30);
-    config.headers.Authorization = `Bearer ${keycloak.token}`;
-  }
-  return config;
+    if (keycloak.authenticated) {
+        await keycloak.updateToken(30);
+        config.headers.Authorization = `Bearer ${keycloak.token}`;
+    }
+    return config;
 }, (error) => {
-  return Promise.reject(error);
+    return Promise.reject(error);
 });
 
 export default api;
--- src/services/keycloak.js
+++ src/services/keycloak.js
@@ -1,9 +1,9 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: `http://${import.meta.env.VITE_BACKEND_SERVER}:${import.meta.env.VITE_KEYCLOAK_PORT}`,
-  realm: "travel-realm",
-  clientId: "travel-frontend",
+  url: import.meta.env.VITE_KEYCLOAK_URL,
+  realm: import.meta.env.VITE_KEYCLOAK_REALM,
+  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
 });
 
 export default keycloak;
