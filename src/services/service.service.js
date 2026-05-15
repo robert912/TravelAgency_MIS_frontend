f4c3706ca31ba7e729b82d2df9abb50b9086e032
Roberto Orellana Tamayo <roberto.orellana.t@usach.cl>
2026-05-15 16:39:53 -0400

    limpiar codigo

--- src/services/service.service.js
+++ src/services/service.service.js
@@ -5,6 +5,5 @@ const getAllActive = () => httpClient.get('/api/services/active');
 const get = id => httpClient.get(`/api/services/${id}`);
 const create = data => httpClient.post("/api/services/", data);
 const update = data => httpClient.put('/api/services/', data);
-const remove = id => httpClient.delete(`/api/services/${id}`);
 
 export default { getAll, getAllActive, get, create, update, remove };
