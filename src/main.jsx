5e118972abbf9c370761d4824f5cdb38fbb54e2f
Roberto <roberto.orellana.t@usach.cl>
2026-05-14 21:44:00 -0400

    update main.jsx eliminar path

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,27 @@ import { ReactKeycloakProvider } from "@react-keycloak/web";
 import keycloak from "./services/keycloak";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ReactKeycloakProvider authClient={keycloak}>
-    <App />
-  </ReactKeycloakProvider>
+    <ReactKeycloakProvider
+        authClient={keycloak}
+        initOptions={{
+            onLoad: "check-sso",
+            pkceMethod: "S256",
+        }}
+        onTokens={() => {
+            if (
+                window.location.hash.includes("code=") ||
+                window.location.hash.includes("state=") ||
+                window.location.search.includes("code=") ||
+                window.location.search.includes("state=")
+            ) {
+                window.history.replaceState(
+                    {},
+                    document.title,
+                    window.location.origin + window.location.pathname
+                );
+            }
+        }}
+    >
+        <App />
+    </ReactKeycloakProvider>
 )
-
